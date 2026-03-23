b188ff8675805efd98060e246689f183aad569b6
Giovanna Muniz <munz.giovanna01@gmail.com>
2026-03-23 16:22:06 -0300

    Exc01 - operacao soma no menu

---
+++ menu.py
@@ -0,0 +1,17 @@
+def menu():
+    print("1. Somar")
+    print("2. Subtrair")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("0. Sair")
+    opcao = int(input("Escolha uma opção: "))
+    return opcao
+ 
+ op = menu()
+print("A opção que você escolheu foi:", op)
+ 
+if op == 1:
+    n1 = float(input("Número 1: "))
+    n2 = float(input("Número 2: "))
+    print(n1 + n2)
+ 
